071aeae0ff6e5c5da9f51fc1a64e9be15b6ccc9e
Zayant21 <zayantofeeq@gmail.com>
2021-12-17 05:46:50 -0800

    commit

--- Subscription/settings.py
+++ Subscription/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'Subscrap/static/'),
 )
 
 
