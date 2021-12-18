9beae84f3318a198de4452b5d6cef6ee52cb7afb
Zayant21 <zayantofeeq@gmail.com>
2021-12-18 04:30:53 -0800

    commit

--- Subscription/settings.py
+++ Subscription/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Subscription.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Subscrap/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
