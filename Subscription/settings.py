4a429f49be92b0f166a910e7f0265e84bfbc8694
Zayant21 <zayantofeeq@gmail.com>
2021-12-18 05:25:48 -0800

    commit

--- Subscription/settings.py
+++ Subscription/settings.py
@@ -79,18 +79,19 @@ WSGI_APPLICATION = 'Subscription.wsgi.application'
 
 DATABASES = {
     'default': {
-    #  'ENGINE': 'django.db.backends.sqlite3',
-    #   'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
     
-        'NAME': 'Subscrap',
-        'USER': 'zayant21',
-        'PASSWORD':'929268',
-        'HOST':'localhost',
-        'PORT': '3306',
+        #'NAME': 'Subscrap',
+        #'USER': 'zayant21',
+        #'PASSWORD':'929268',
+        #'HOST':'localhost',
+        #'PORT': '3306',
         #'OPTIONS': {
         #    'sql_mode' : 'traditional',
     }
 }
+
 
 
 
